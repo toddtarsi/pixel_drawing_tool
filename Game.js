d9ffbaa6302e61994cf9225dee23321775b170d7
toddtarsi <toddtarsi@yahoo.com>
2014-07-18 09:06:29 -0500

    Fixed issues, made sure this is a pretty DRY implementation

--- Game.js
+++ Game.js
@@ -21,138 +21,119 @@ var artist = (function(idString){
     return canvas;
   })();
   
-  
   artist.data = (function(){
     var data = {};
     data.currentImage = [];
-    data.init = function initImageData(size){
-      data.size = 'medium';
-      data.width = 20;
-      data.height = 20;
-      for (var i = 0; i < artist.canvasSettings.height; i++) {
-        if (!)data.currentImage.push([]);
-        for (var j = 0; j < artist.canvasSettings.width; j++) {
-          data.currentImage.push('');
+    data.size = 'medium';
+    data.width = 20;
+    data.height = 20;
+    var init = function init(){
+      for (var i = 0; i < data.height; i++) {
+        data.currentImage[i] =  [];
+        for (var j = 0; j < data.width; j++) {
+          data.currentImage[i][j] = '';
         }
       }
-      return true;
-    };
+    }();
     data.refresh = function refreshImageData(size){
       data.size = size;
       data.width = {tall: 10, wide: 30, small: 10, medium: 20, large: 30}[data.size];
       data.height = {tall: 30, wide: 15, small: 10, medium: 20, large: 30}[data.size];
       for (var j = 1; j <= data.height; j++) {
-        if (!data.currentImage[i])data.currentImage.push([]);
-          for (var i = 1; i <= data.width; i++) {
-            if (!data.currentImage[i][j]) data.currentImage[i][j] = '';
-          }
+        if (!data.currentImage[i]) data.currentImage.push([]);
+        for (var i = 1; i <= data.width; i++) {
+          if (!data.currentImage[i][j]) data.currentImage[i][j] = '';
         }
       }
     };
     data.updatePixel = function updatePixelData(i, j, color) {
-      artist.canvasSettings.currentImage[i][j] = color || '';
+      data.currentImage[i][j] = color;
+      artist.draw.pixel(i,j);
     }
     return data;
   })();
   
   artist.draw = (function(){
-    var pixelator = {};
-    pixelator.drawPixel = function drawPixel(i,j){
-      artist.ctx.fillStyle = artist.canvasSettings.bgColor;
-      artist.ctx.fillRect(i*25,j*25,24,24);
+    var draw = {};
+    draw.pixel = function drawPixel(i,j){
+      artist.ctx.fillStyle = artist.data.currentImage[i][j] === '' ? artist.colors.bg : artist.data.currentImage[i][j];
+      artist.ctx.fillRect(i*25 + 25,j*25 + 25,24,24);
     };
-    pixelator.render = function(){ 
-      for (var j = 1; j <= artist.canvasSettings.height; j++) {
-        for (var i = 1; i <= artist.canvasSettings.width; i++) {
-          pixelator.drawPixel(i,j);
+    draw.clear = function clearPixelMap(){
+      artist.ctx.setTransform(1,0,0,1,0,0);
+      artist.ctx.clearRect(0,0, artist.canvas.dimensions[0], artist.canvas.dimensions[1]);
+    };
+    draw.screen = function(){
+      draw.clear();
+      for (var j = 0; j < artist.data.width; j++) {
+        for (var i = 0; i < artist.data.height; i++) {
+          draw.pixel(i,j);
         }
       }
     };
-    return pixelator;
+    draw.screen();
+    return draw;
   })();
   
-  artist.inputs = function(){
+  artist.bindInputs = function(){
     var dragging = false;
+    var erasing = false;
+    
+    $(document).on('contextmenu', function(event){
+      event.preventDefault();
+      erasing = true;
+      return false;
+    });
     
     $('#pxColor').change(function(value){
-      artist.canvasSettings.pxColor = $('#pxColor').val();
+      artist.colors.px = $('#pxColor').val();
     });
 
     $('#bgColor').change(function(value){
-      artist.canvasSettings.bgColor = $('#bgColor').val();
-      updateBackground();
+      artist.colors.bg = $('#bgColor').val();
+      artist.draw.screen();
     });
 
     $('select.gridDimensions').change(function(){
       var newSize = $('.gridDimensions').val();
-      artist.canvasSettings.size = newSize;
-      clearPixelMap();
-      updateImageDataArrayDim();
-      updateBackground();
+      artist.data.refresh(newSize);
+      artist.draw.screen();
     });
 
-    $(idString).mousedown(function(event){
+    $('#' + idString).mousedown(function(event){
       event.preventDefault();
-      artist.canvasSettings.dragging = true;
-      return false;
+      dragging = true;
+      var xPixel = Math.floor(event.offsetX/25) - 1;
+      var yPixel = Math.floor(event.offsetY/25) - 1;
+      if(event.button==2) erasing = true;
+      if (xPixel >= 0 && xPixel < artist.data.height && yPixel >= 0 && yPixel < artist.data.height ) {
+        if (erasing) artist.data.updatePixel(xPixel,yPixel, '');
+        else artist.data.updatePixel(xPixel,yPixel, artist.colors.px);
+      }
+      if(event.which==2) return false;
     });
 
-    $(idString).mousemove(function(event){
+    $('#' + idString).mousemove(function(event){
       event.preventDefault();
       if (dragging) {
-        var xPixel = Math.floor(event.offsetX/25);
-        var yPixel = Math.floor(event.offsetY/25);
-        if (xPixel > 0 && xPixel <= artist.canvasSettings.height && yPixel > 0 && yPixel <= artist.canvasSettings.height ) {
-          artist.data.updatePixel(xPixel,yPixel);    
+        var xPixel = Math.floor(event.offsetX/25) - 1;
+        var yPixel = Math.floor(event.offsetY/25) - 1;
+        if (xPixel >= 0 && xPixel < artist.data.height && yPixel >= 0 && yPixel < artist.data.height ) {
+          if (erasing) artist.data.updatePixel(xPixel,yPixel, '');
+          else artist.data.updatePixel(xPixel,yPixel, artist.colors.px);
         }
       }
     });
 
-    $(idString).mouseup(function(event){
+    $('#' + idString).mouseup(function(event){
       event.preventDefault();
-      artist.canvasSettings.dragging = false;
+      dragging = false;
+      erasing = false;
     });
   }();
   
   return artist;
 })('gameCanvas');
 
-function updateImageDataArrayDim(){
-  artist.canvasSettings.width = {tall: 10, wide: 30, small: 10, medium: 20, large: 30}[artist.canvasSettings.size];
-  artist.canvasSettings.height = {tall: 30, wide: 15, small: 10, medium: 20, large: 30}[artist.canvasSettings.size];
-  for (var j = 1; j <= artist.canvasSettings.height; j++) {
-    if (!artist.canvasSettings.currentImage[i])artist.canvasSettings.currentImage.push([]);
-    for (var i = 1; i <= artist.canvasSettings.width; i++) {
-      if (!artist.canvasSettings.currentImage[i][j]) artist.canvasSettings.currentImage[i][j] = '';
-    }
-  }
-}
+$('#bgColor').val('#FFFFFF').change();
 
-function updateBackground(){
-  function drawPixel(i,j){
-    artist.ctx.fillStyle= artist.canvasSettings.bgColor;
-    artist.ctx.fillRect(i*25,j*25,24,24);
-  };
-  for (var j = 0; j < artist.canvasSettings.height; j++) {
-    for (var i = 0; i < artist.canvasSettings.width; i++) {
-      if (artist.canvasSettings.currentImage[i][j] === '') drawPixel(i+1,j+1);
-    }
-  }
-}
-
-function clearPixelMap(){
-  artist.ctx.setTransform(1,0,0,1,0,0);
-  artist.ctx.clearRect(0,0, artist.Canvas.width, artist.Canvas.height);
-}
-
-
-function updatePixel(i,j){
-  if (artist.canvasSettings.pxColor !== artist.canvasSettings.currentImage[i-1][j-1]) {
-    artist.canvasSettings.currentImage[i-1][j-1] = artist.canvasSettings.pxColor;
-    artist.ctx.fillStyle= artist.canvasSettings.pxColor;
-    artist.ctx.fillRect(i*25,j*25,24,24);
-  }
-}
-
-initImageData();
-blankPixelMap('medium');
